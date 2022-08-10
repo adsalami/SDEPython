fd222c7f695b4808abd77fc876a3b4544a4bf150
adsalami <adsalami@users.noreply.github.com>
2022-08-10 15:05:39 +0000

    learning about input function

--- myscript.sh
+++ myscript.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
+# Don't forget to comment
 echo adduser
 read username
 echo add password
 read password
 echo what group
 read group
-useradd -p $password $username
-usermod -aG $group $username
 
+sudo useradd -p $password $username
+sudo addgroup $group
+sudo usermod -aG $group $username
