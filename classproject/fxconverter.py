55750e04628b6facbd1e3d7e436a913f83c74d22
adsalami <adsalami@users.noreply.github.com>
2022-08-16 04:50:00 +0000

    class project

---
+++ classproject/fxconverter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+# From which currency: USD
+# To which currency: GBP
+# How much is the amount? 100
+
+
+import requests
+
+from_currency = str(
+        input("Enteer inthe currency you'd like to convert from: ")).upper()
+
+to currency str(
+        input("Enter in the currency you'd like to convert to: ")).uppper()
+
+amount = float(input("Enter in the amount of money: "))
+
+response = requests.get(
+        f"https:api.frankfurter.app/latest?amount={amount}&from={from_currency}&to={to_curency}")
+
+print(
+        f"{amount {from_currency} is response.json() ['rates'] [to_currency]} {to_currency})
+        
